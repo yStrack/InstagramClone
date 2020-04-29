f65b29467975451de1ca378b10cdb5c6ad33f08c
Yuri Strack <yuri.strack@gmail.com>
2020-04-29 16:46:38 -0300

    Customizing table cells

--- Instagram-Clone/Controllers/HomeViewController.swift
+++ Instagram-Clone/Controllers/HomeViewController.swift
@@ -32,9 +32,14 @@ class HomeViewController: UIViewController {
 
 }
 
+// TableView extension
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 600
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,14 +49,36 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
         cell.set(post: post)
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        let separatorView = UIView(frame: CGRect(x:0, y: headerView.frame.height, width: tableView.frame.width, height: 1))
+        separatorView.backgroundColor = UIColor.separator
+        headerView.addSubview(separatorView)
+        return headerView
+    }
+}
+
+// CollectionView extension
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
 }
 
 extension HomeViewController{
     func fetchData() -> [Post]{
-        let user1 = User(name: "Yuri Strack", profilePic: UIImage(named: "profile-pic")!)
+        let user1 = User(name: "yuri_strack", profilePic: UIImage(named: "profile-pic")!)
         let post1 = Post(user: user1, location: "Rio de Janeiro", image: UIImage(named: "InstaPost")!, likes: "Curtido por Alex", description: "CarnaVRAU")
+        let post2 = Post(user: user1, location: "Rio de Janeiro", image: UIImage(named: "InstaPost")!, likes: "Curtido por Alex", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+        let post3 = Post(user: user1, location: "Rio de Janeiro", image: UIImage(named: "InstaPost")!, likes: "Curtido por Alex", description: "CarnaVRAU")
 //        print(user1)
 //        print(post1)
-        return [post1]
+        return [post1,post2,post3]
     }
 }
--- Instagram-Clone/Views/PostTableViewCell.swift
+++ Instagram-Clone/Views/PostTableViewCell.swift
@@ -19,11 +19,12 @@ class PostTableViewCell: UITableViewCell {
     @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var likesLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UITextView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        configCell()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -32,13 +33,48 @@ class PostTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func configCell(){
+        self.selectionStyle = .none
+        
+        moreButton.setImage(UIImage(named: "menu") , for: .normal)
+        
+        postImage.contentMode = .scaleAspectFill
+        postImage.clipsToBounds = true
+        userImage.layer.borderWidth = 2;
+        userImage.layer.borderColor = UIColor.white.cgColor
+        userImage.layer.cornerRadius = 30
+        userImage.layer.masksToBounds = true
+        userImage.clipsToBounds = true
+        userImage.layer.cornerRadius = userImage.frame.size.height/2
+    }
+    
     func set(post: Post){
         self.userName.text = post.user.name
         self.userImage.image = post.user.profilePic
         self.postLocation.text = post.location ?? ""
         self.postImage.image = post.image
         self.likesLabel.text = post.likes
-        self.descriptionLabel.text = post.description
+        
+        let range = NSMakeRange(post.user.name.count, post.description.count+1)
+        self.descriptionLabel.attributedText = attributedString(from: post.user.name + " " + post.description, nonBoldRange: range)
     }
 
+}
+
+extension PostTableViewCell{
+    func attributedString(from string: String, nonBoldRange: NSRange?) -> NSAttributedString {
+        let fontSize = UIFont.systemFontSize
+        let attrs = [
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: fontSize),
+            NSAttributedString.Key.foregroundColor: UIColor.black
+        ]
+        let nonBoldAttribute = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize),
+        ]
+        let attrStr = NSMutableAttributedString(string: string, attributes: attrs)
+        if let range = nonBoldRange {
+            attrStr.setAttributes(nonBoldAttribute, range: range)
+        }
+        return attrStr
+    }
 }
